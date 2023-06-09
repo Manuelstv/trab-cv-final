0fbafd76ee9a00bf58923a410acb28f7f7571cba
mstveras <manuel.stveras@gmail.com>
2023-06-09 16:16:34 -0300

    init commit

--- example_baterias_alvaro.py
+++ example_baterias_alvaro.py
@@ -6,73 +6,32 @@ from torch import nn
 import torch.nn.functional as F
 import numpy as np
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
 from torchvision import transforms
 import torchvision.datasets as datasets
-from equi_conv import EquiConv2d, equi_conv2d
 torch.cuda.empty_cache()
-
-
-class SphereTat(nn.Module):
-    def __init__(self):
-        super(SphereTat, self).__init__()
-        self.conv1 = EquiConv2d(3, 3*2, stride=1)
-        self.pool1 = SphereMaxPool2D(stride=2)
-        self.conv2 = EquiConv2d(3*2, 3*4, stride=1)
-        self.pool2 = SphereMaxPool2D(stride=2)
-        self.conv3 = EquiConv2d(3*4, 3*8, stride=1)
-        self.pool3 = SphereMaxPool2D(stride=2)
-        self.conv4 = EquiConv2d(3*8, 3*16, stride=1)
-        self.pool4 = SphereMaxPool2D(stride=2)
-        self.conv5 = EquiConv2d(3*16, 3*32, stride=1)
-        self.pool5 = SphereMaxPool2D(stride=2)
-        self.conv6 = EquiConv2d(3*32, 3*64, stride=1)
-        self.pool6 = SphereMaxPool2D(stride=2)
-        self.conv7 = EquiConv2d(3*64, 3*128, stride=1)
-        self.pool7 = SphereMaxPool2D(stride=2)
-        self.conv8 = EquiConv2d(3*128, 3*256, stride=1)
-        self.pool8 = SphereMaxPool2D(stride=2)
-        self.conv9 = EquiConv2d(3*256, 3*512, stride=1)
-        self.pool9 = SphereMaxPool2D(stride=2)
-        self.fully = nn.Sequential(nn.Linear(8064, 16128), nn.Dropout(0.5), nn.Linear(16128, 2))
-
-    def forward(self, x):
-        x = F.relu(self.pool1(self.conv1(x)))
-        x = F.relu(self.pool2(self.conv2(x)))
-        x = F.relu(self.pool3(self.conv3(x)))
-        x = F.relu(self.pool4(self.conv4(x)))
-        x = F.relu(self.pool5(self.conv5(x)))
-        x = F.relu(self.pool6(self.conv6(x)))
-        x = F.relu(self.pool7(self.conv7(x)))
-        #x = F.relu(self.pool8(self.conv8(x)))
-        #x = F.relu(self.pool9(self.conv9(x)))
-        print(x.size())
-        x = x.view(-1, 8064)  # flatten, [B, C, H, W) -> (B, C*H*W)
-        x = self.fully(x)        
-        return x 
 
 class SphereNet(nn.Module):
     def __init__(self):
         super(SphereNet, self).__init__()
-        self.conv1 = SphereConv2D(3, 3*2, stride=1)
+        self.conv1 = SphereConv2D(1, 32, stride=1)
         self.pool1 = SphereMaxPool2D(stride=2)
-        self.conv2 = SphereConv2D(3*2, 3*4, stride=1)
+        self.conv2 = SphereConv2D(32, 64, stride=1)
         self.pool2 = SphereMaxPool2D(stride=2)
-        self.conv3 = SphereConv2D(3*4, 3*8, stride=1)
+        self.conv3 = SphereConv2D(64, 128, stride=1)
         self.pool3 = SphereMaxPool2D(stride=2)
-        self.conv4 = SphereConv2D(3*8, 3*16, stride=1)
+        self.conv4 = SphereConv2D(128, 256, stride=1)
         self.pool4 = SphereMaxPool2D(stride=2)
-        self.conv5 = SphereConv2D(3*16, 3*32, stride=1)
+        self.conv5 = SphereConv2D(256, 512, stride=1)
         self.pool5 = SphereMaxPool2D(stride=2)
-        self.conv6 = SphereConv2D(3*32, 3*64, stride=1)
+        self.conv6 = SphereConv2D(512, 1024, stride=1)
         self.pool6 = SphereMaxPool2D(stride=2)
-        self.conv7 = SphereConv2D(3*64, 3*128, stride=1)
+        self.conv7 = SphereConv2D(1024, 2048, stride=1)
         self.pool7 = SphereMaxPool2D(stride=2)
-        self.conv8 = SphereConv2D(3*128, 3*256, stride=1)
+        self.conv8 = SphereConv2D(2048, 4096, stride=1)
         self.pool8 = SphereMaxPool2D(stride=2)
-        self.conv9 = SphereConv2D(3*256, 3*512, stride=1)
+        self.conv9 = SphereConv2D(4096, 4096, stride=1)
         self.pool9 = SphereMaxPool2D(stride=2)
-        self.fully = nn.Sequential(nn.Linear(3072, 6144), nn.Dropout(0.5), nn.Linear(6144, 2))
+        self.fully = nn.Sequential(nn.Linear(8192, 4096), nn.Dropout(0.5), nn.Linear(4096, 2))
 
     def forward(self, x):
         x = F.relu(self.pool1(self.conv1(x)))
@@ -84,8 +43,7 @@ class SphereNet(nn.Module):
         x = F.relu(self.pool7(self.conv7(x)))
         x = F.relu(self.pool8(self.conv8(x)))
         x = F.relu(self.pool9(self.conv9(x)))
-        #print(x.size())
-        x = x.view(-1, 3072)  # flatten, [B, C, H, W) -> (B, C*H*W)
+        x = x.view(-1, 8192)  # flatten, [B, C, H, W) -> (B, C*H*W)
         x = self.fully(x)        
         return x 
          
@@ -98,12 +56,12 @@ class Net(nn.Module):
         self.conv4 = nn.Conv2d(3*8, 3*16, kernel_size=3, padding=1)
         self.conv5 = nn.Conv2d(3*16, 3*32, kernel_size=3, padding=1)
         self.conv6 = nn.Conv2d(3*32, 3*64, kernel_size=3, padding=1)
-        self.conv7 = nn.Conv2d(3*64, 3*128, kernel_size=3, padding=1)
-        self.conv8 = nn.Conv2d(3*128, 3*256, kernel_size=3, padding=1)
-        self.conv9 = nn.Conv2d(3*256, 3*512, kernel_size=3, padding=1)
+        #self.conv7 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv8 = nn.Conv2d(2048, 4096, kernel_size=3, padding=1)
+        self.conv9 = nn.Conv2d(4096, 4096, kernel_size=3, padding=1)
         #self.conv6 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
         #self.fc = nn.Linear(8192, 4096)
-        self.fully = nn.Sequential(nn.Linear(3072, 6144), nn.Dropout(0.5), nn.Linear(6144, 2))
+        self.fully = nn.Sequential(nn.Linear(9408, 4704), nn.Dropout(0.5), nn.Linear(4704, 4))
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
@@ -112,12 +70,12 @@ class Net(nn.Module):
         x = F.relu(F.max_pool2d(self.conv4(x), 2))
         x = F.relu(F.max_pool2d(self.conv5(x), 2)) 
         x = F.relu(F.max_pool2d(self.conv6(x), 2))
-        x = F.relu(F.max_pool2d(self.conv7(x), 2))
+        #x = F.relu(F.max_pool2d(self.conv7(x), 2))
         #x = F.relu(F.max_pool2d(self.conv8(x), 2)) 
         #x = F.relu(F.max_pool2d(self.conv9(x), 2))
         
         #print(x.size())
-        x = x.view(-1, 3072) 
+        x = x.view(-1, 9408) 
         x = self.fully(x)
         #print(x.size())
         return x
@@ -177,11 +135,11 @@ def test(args, model, device, test_loader):
                 if(y_pred[n]==target[n]):
                     correct += 1
 
-            #print(y_pred, target)
+            #y_pred = y_pred.squeeze(1)
+            #pred = y_pred.max(1, keepdim=True)[1] # get the index of the max log-probability
 
     test_loss /= len(test_loader.dataset)
-    print('\n Test Loss: ', test_loss)
-    print('\n Accuracy: ', correct/len(test_loader.dataset))
+    print(correct/len(test_loader.dataset))
     
     return(test_loss)
 
@@ -228,12 +186,12 @@ def main():
         
     train_dataset = datasets.ImageFolder(
     #root='/media/bycrop/byCrop_Dataset/SUN3601024x512/pano1024x512/train',
-    root = '/media/bycrop/byCrop_Dataset/SUN3601024x512/pano1024x512/train',
+    root = '/media/bycrop/byCrop_Dataset/dataset_baterias/dataset/train',
     transform=transform
     )
 
     test_dataset = datasets.ImageFolder(
-    root='/media/bycrop/byCrop_Dataset/SUN3601024x512/pano1024x512/val',
+    root='/media/bycrop/byCrop_Dataset/dataset_baterias/dataset/val',
     transform=transform
     )
 
@@ -249,49 +207,31 @@ def main():
     )
 
     # Train
-    sphere_model = SphereTat().to(device)
+    #sphere_model = SphereNet().to(device)
     model = Net().to(device)
     
     if args.optimizer == 'adam':
-        sphere_optimizer = torch.optim.Adam(sphere_model.parameters(), lr=args.lr)
+        #sphere_optimizer = torch.optim.Adam(sphere_model.parameters(), lr=args.lr)
         optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
     elif args.optimizer == 'sgd':
-        sphere_optimizer = torch.optim.SGD(sphere_model.parameters(), lr=args.lr, momentum=args.momentum)
+        #sphere_optimizer = torch.optim.SGD(sphere_model.parameters(), lr=args.lr, momentum=args.momentum)
         optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
     
     loss_train=[]
-    loss_train_std=[]
+    loss_train_cnn=[]
     loss_test=[]
-    loss_test_std=[]
+    loss_test_cnn=[]
 
     for epoch in range(1, args.epochs + 1):
         ## SphereCNN
-        print('{} Sphere CNN {}'.format('='*10, '='*10))
-        loss_train.append(train(args, sphere_model, device, train_loader, sphere_optimizer, epoch))
-        loss_test.append(test(args, sphere_model, device, test_loader))
+        #print('{} Sphere CNN {}'.format('='*10, '='*10))
+        #print(train(args, sphere_model, device, train_loader, sphere_optimizer, epoch))
+        #print(test(args, sphere_model, device, test_loader))
 
         # Conventional CNN
         print('{} Conventional CNN {}'.format('='*10, '='*10))
-        loss_train_std.append(train(args, model, device, train_loader, optimizer, epoch))
-        loss_test_std.append(test(args, model, device, test_loader))
-
-
-    #informar o vetor de Ã©pocas no lugar no np.array(list.........
-    epochs = np.array(list(range(0,2*len(loss_train),2)))
-
-
-    plt.figure(figsize=(10, 5))
-
-    plt.plot(epochs, loss_train, 'r', label='Training Loss') # plotting t, a separately 
-    plt.plot(epochs, loss_test, 'b', label='Validation Loss') # plotting t, b separately 
-    plt.plot(epochs, loss_train_std, 'g', label='Training Loss std') # plotting t, a separately 
-    plt.plot(epochs, loss_test_std, 'y', label='Validation Loss std') # plotting t, b separately 
-    plt.legend(loc='upper right')
-    plt.grid(linestyle=':')
-    plt.xlabel('Epochs',fontsize=18)
-    plt.ylabel('Loss',fontsize=18)
-
-    plt.show()
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader)
 
 
 
